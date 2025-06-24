cb6d18b35def5b360d96f7c172b4f516ea972b8f
srlimaz <alvarolbarros@hotmail.com>
2025-06-24 20:58:05 -0300

    cria programa que calcula o volume de uma esfera

---
+++ sphere.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void	volume_calculation(double raio, double *volume_result)
+{
+	*volume_result = (4/3.0) * 3.14159 * (raio * raio * raio);
+}
+
+int	main()
+{
+	int raio;
+	double volume;
+
+	// Entrada
+	scanf("%d", &raio);
+
+	// Processamento
+	volume_calculation(raio, &volume);
+
+	// Saída
+	printf("VOLUME = %.3f\n", volume);
+	return 0;
+}
