c44cbb91bbe906d0759883b84486e90f03df571e
Lima Linux <alvarolbarros@hotmail.com>
2025-06-27 18:10:10 -0300

    comenta ex distance, detalhando sobre cada função

--- distance_between_two_points.c
+++ distance_between_two_points.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// Função de raiz quadrada
 double square_root(double number) 
 {
 	double x = number;
@@ -13,7 +14,7 @@ double square_root(double number)
 	return x;
 }
 
-
+// Função para calcular a distancia entre dois pontos
 void	calc_distance_points(double point_x1, double point_y1, double point_x2, double point_y2, double *distance)
 {
 	double result_root;
@@ -26,11 +27,14 @@ int	main()
 {
 	double x1, y1, x2, y2, result;
 
+	// Entrada
 	scanf("%lf %lf", &x1, &y1);
 	scanf("%lf %lf", &x2, &y2);
 
+	// Processamento
 	calc_distance_points(x1, y1, x2, y2, &result);
 
+	// Saída
 	printf("%.4f\n", result);
 	return 0;
 }
