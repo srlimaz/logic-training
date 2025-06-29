2d8c72b75d31ecb2b4244784d35d6fc70be11b7d
srlimaz <alvarolbarros@hotmail.com>
2025-06-28 21:41:33 -0300

    conclui exercicio de calculo de distancia entre dois carros

---
+++ car_distance.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int	main()
+{
+	int entry;
+	int result;
+
+	// Entrada
+	scanf("%d", &entry);
+
+	// Processamento
+	result = entry * 2;
+
+	// Saída
+	printf("%d minutos\n", result);
+	return 0;
+}
